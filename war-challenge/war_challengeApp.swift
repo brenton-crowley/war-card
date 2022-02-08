1d22568650e92310e0a4e69a2f0f1fc97066cd26
Franklin <training1@franklin.training>
2022-02-08 15:45:41 +1100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Christopher Ching on 2020-10-28.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
